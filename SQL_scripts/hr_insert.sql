6ee1afc6167e73e356c59472d2952b763d2395c1
GGTalbros <gaurav@talbrosaxles.com>
2023-03-31 15:51:27 +0530

    Complete insert statement

--- SQL_scripts/hr_insert.sql
+++ SQL_scripts/hr_insert.sql
@@ -1,3 +1,4 @@
 GO
-Insert into tal_hr_mail values('CFT','cft@talbrosaxles.com','Recruitment MIS',Null,Null,'11:00','12:00','Monday','A')
+
+Insert into tal_hr_mail values('CFT','cft@talbrosaxles.com','Recruitment MIS.xlsx','Please find attached Recruitment MIS','Recruitment MIS','11:00','12:00','Monday','A')
 GO
